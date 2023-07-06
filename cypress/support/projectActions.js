2169b15c15cabb57c423dd2c714e715590440097
9evorg <gevorg.mamikonyan@pipe17.com>
2023-07-07 03:38:57 +0400

    added negative boundary case via api

--- cypress/support/projectActions.js
+++ cypress/support/projectActions.js
@@ -27,6 +27,13 @@ class ProjectActions {
         });
     }
 
+    verifyUserCannotCreateMoreProjects = () => {
+        projectAPI.createProject('boundary project').then((response) => {
+            expect(response.status).to.equal(403);
+            // Add additional assertions or logic based on the expected 403 response
+          });
+    }
+
     verifyTaskIsSavedViaAPI = (taskName, taskDescription) => {
         projectAPI.getAllProjects()
             .then(allProjects => {
